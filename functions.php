98b2cabc9f9904e181c7795c5126e4974b315a3a
Carlos Zapata <cdanielzt@gmail.com>
2021-07-18 18:09:54 -0500

    agregar cabecera

--- functions.php
+++ functions.php
@@ -17,6 +17,15 @@ function load_javascript(){
 
 add_action('wp_enqueue_scripts','load_javascript');
 
+//Add Google Fonts
+function wpb_add_google_fonts() {
+ 
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800&display=swap', false ); 
+}
+     
+    add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+    
+
 //Add Menus
 add_theme_support( 'menus' );
 
