40604fe2078f8a266ae99acf1a764669ae2c1872
Carlos Zapata <cdanielzt@gmail.com>
2021-07-24 13:28:06 -0500

    Agregar loop a page.php

--- page.php
+++ page.php
@@ -6,5 +6,19 @@
 
 </div>
 
+<section class="content">
+
+<div class="container">
+
+    <?php if(have_posts()) : while(have_posts()) : the_post();?>
+
+    <?php the_content(); ?>
+    
+    <?php endwhile; else: endif;?>
+</div>
+
+</section>
+
+<?php get_footer() ?>
 
 <?php get_footer() ?>
