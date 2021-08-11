4161342054d5ba02919e45fead1dcc57455bce33
Carlos Zapata <cdanielzt@gmail.com>
2021-08-10 19:34:02 -0500

    agregar responsive

--- single.php
+++ single.php
@@ -1,42 +1,30 @@
 <?php get_header() ?>
 
-
-
-
 <section class="content">
 
-<div class="container">
-   
-    <div class="row">
-        
+<div class="container mt-4">
+        <div class="row">
             
-        
-        <div class="col-lg-3">
-            <div class="sticky-top">
-            <?php get_sidebar(); ?>
-            </div>
-        </div>
-
-        <div class="col-lg-9">
+      
+        <div class="col-md-6 col-12">
         <a href="<?php the_permalink()?>;">
             <?php if(has_post_thumbnail()):?>
                 <img src="<?php the_post_thumbnail_url( 'post_image' ); ?>" alt="<?php the_title(); ?>" class="img-fluid">
             <?php endif;?>
         </a>
+        </div>
+        <div class="col-md-6 col-12">
         <h1><?php the_title(); ?></h1>
 
         <?php if(have_posts()) : while(have_posts()) : the_post();?>
 
         <?php the_content(); ?>
-
+        <?php get_template_part('template-parts/post','navigation')?>
         <?php endwhile; else: endif;?>
         </div>
-
+        </div>
+    
     </div>
-
-   
-
-</div>
 
 </section>
 
