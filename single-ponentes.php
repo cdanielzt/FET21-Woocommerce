9c28801e5e7d909fba31ca4a6d7c2dde6bb7c696
cdanielzt <cdanielzt@gmail.com>
2021-08-16 16:00:31 -0500

    agregados single page view para conferencias y ponentes

---
+++ single-ponentes.php
@@ -0,0 +1,31 @@
+<?php get_header() ?>
+
+<section class="content">
+
+<div class="container mt-4">
+        <div class="row">
+            
+      
+        <div class="col-md-6 col-12">
+        <a href="<?php the_permalink()?>;">
+            <?php if(has_post_thumbnail()):?>
+                <img src="<?php the_post_thumbnail_url( 'post_image' ); ?>" alt="<?php the_title(); ?>" class="img-fluid">
+            <?php endif;?>
+        </a>
+        </div>
+        <div class="col-md-6 col-12">
+        <h1><?php the_title(); ?></h1>
+
+        <?php if(have_posts()) : while(have_posts()) : the_post();?>
+
+        <?php the_content(); ?>
+        <?php get_template_part('template-parts/post','navigation')?>
+        <?php endwhile; else: endif;?>
+        </div>
+        </div>
+    
+    </div>
+
+</section>
+
+<?php get_footer() ?>
